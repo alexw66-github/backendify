b228906dfffa700800262ddb8e5029d0815e971d
Alex Wilson <online.account@gmx.com>
2022-08-02 16:12:06 +0100

    fix: nginx debugging

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -x
 
+echo NGINX_PORT_HTTP=${NGINX_PORT_HTTP}
+echo SERVICE_PORT_HTTP=${SERVICE_PORT_HTTP}
+echo ASPNETCORE_URLS=${ASPNETCORE_URLS}
+
+export ASPNETCORE_URLS=${ASPNETCORE_URLS}
+
 nginx #nginx -g 'daemon off;'
 dotnet "Backendify.Api.dll"
